9e2fea4e3c9d5450a003793268ae7b501795587e
PIBOP0117003 Кистанов Роман Александрович <PIBOP0117003@sibupk.su>
2019-11-16 13:17:00 +0700

    Added DateFunctions class

---
+++ LeapYears/LeapYears/DateFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeapYears
+{
+    static class DateFunctions
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                return true; //true - если год високосный, иначе false
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
