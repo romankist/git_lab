602a36bedd56614d604e3f9a913f57095c7da738
PIBOP0117003 Кистанов Роман Александрович <PIBOP0117003@sibupk.su>
2019-11-16 13:06:11 +0700

    Created Form

--- LeapYears/LeapYears/Form1.Designer.cs
+++ LeapYears/LeapYears/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LeapYears
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_Input = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(92, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверить!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label_Input
+            // 
+            this.label_Input.AutoSize = true;
+            this.label_Input.Location = new System.Drawing.Point(50, 48);
+            this.label_Input.Name = "label_Input";
+            this.label_Input.Size = new System.Drawing.Size(75, 13);
+            this.label_Input.TabIndex = 1;
+            this.label_Input.Text = "Укажите год:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label_Result
+            // 
+            this.label_Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_Result.AutoSize = true;
+            this.label_Result.Location = new System.Drawing.Point(117, 157);
+            this.label_Result.Name = "label_Result";
+            this.label_Result.Size = new System.Drawing.Size(0, 13);
+            this.label_Result.TabIndex = 3;
+            this.label_Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label_Result);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_Input);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Проверка високосности";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_Input;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_Result;
     }
 }
 
